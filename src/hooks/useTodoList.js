ac66b14d0e33aeda2dd752f583912f02bb5f71ff
jeancmr <jean.madiedo08@gmail.com>
2025-07-04 11:43:15 -0500

    feat: add localStorage in useTodoList

--- src/hooks/useTodoList.js
+++ src/hooks/useTodoList.js
@@ -1,9 +1,18 @@
-import { useReducer } from 'react';
-import todoListsData from '../data/data';
+import { useEffect, useReducer } from 'react';
 import { todoListReducer } from '../utils/todoListReducer';
 
+const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todoLists')) || [];
+};
+
 export const useTodoList = (handleCloseModal) => {
-  const [todoLists, dispatch] = useReducer(todoListReducer, todoListsData);
+  const [todoLists, dispatch] = useReducer(todoListReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem('todoLists', JSON.stringify(todoLists));
+  }, [todoLists]);
 
   const handleAddList = (list) => {
     const action = {
