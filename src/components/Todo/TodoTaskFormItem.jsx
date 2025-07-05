263bc78d375c4f6f287648a2ac46f9217087e128
jeancmr <jean.madiedo08@gmail.com>
2025-07-04 21:25:30 -0500

    style: toggle item by clicking text

--- src/components/Todo/TodoTaskFormItem.jsx
+++ src/components/Todo/TodoTaskFormItem.jsx
@@ -1,8 +1,20 @@
 export const TodoTaskFormItem = ({ task, onToggleTask, onDeleteTask }) => {
   return (
     <li className="text-lg flex gap-2 text-white">
-      <input type="checkbox" checked={task.completed} onChange={() => onToggleTask(task.id)} />
-      <span className={` text-lie ${task.completed ? 'line-through' : ''}`}>{task.name}</span>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        name="todoCheckbox"
+        onChange={() => onToggleTask(task.id)}
+        className="cursor-pointer mr-2"
+      />
+      <span
+        className={`cursor-default ${task.completed ? 'line-through' : ''}`}
+        onClick={() => onToggleTask(task.id)}
+      >
+        {task.name}
+      </span>
+
       <button className="ml-auto mr-4 cursor-pointer" onClick={() => onDeleteTask(task.id)}>
         &#x2716;
       </button>
