dfed4e6808e63361f12f2f17b7c7d67a1ec2815f
jeancmr <jean.madiedo08@gmail.com>
2025-07-04 23:02:13 -0500

    style: change header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from './NavBar';
 
 const Header = () => {
   return (
-    <header className="header-app">
+    <header className="max-w-7xl mx-auto flex justify-between items-center py-4 px-6 my-4 rounded-lg bg-neutral-900 text-white">
       <Logo />
       <NavBar />
     </header>
